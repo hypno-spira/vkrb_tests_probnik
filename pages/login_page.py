199a511a2654c5f98baba5658b0f06ea372d5a0d
hypno-spira <64416493+hypno-spira@users.noreply.github.com>
2021-04-29 00:59:33 +0700

    add half of third test - register_with_invalid_sponsor_id

--- pages/login_page.py
+++ pages/login_page.py
@@ -36,3 +36,25 @@ class LoginPage(BasePage):
         message_text = (shadow_dom.find_element_by_css_selector("div.alert.alert-danger")).text
         assert message_text == "Wrong password", f"Появившееся сообщение - {message_text}"
 
+    def register_on_the_site(self, sponsor_id, first_name, last_name, email, browser):
+        shadow_dom = self.expand_shadow_element(browser.find_element_by_tag_name("v-api2-middleware"))
+        button_sign_up = shadow_dom.find_element_by_css_selector("#formLogin>div>h3>span.header-buttons>a")
+        button_sign_up.click()
+        time.sleep(3)
+        sponsor_id_input = shadow_dom.find_element_by_css_selector("#lf-ref")
+        first_name_input = shadow_dom.find_element_by_css_selector("#lf-4")
+        last_name_input = shadow_dom.find_element_by_css_selector("#lf-5")
+        email_input = shadow_dom.find_element_by_css_selector("#lf-6")
+        sponsor_id_input.send_keys(sponsor_id)
+        first_name_input.send_keys(first_name)
+        last_name_input.send_keys(last_name)
+        email_input.send_keys(email)
+        button_register = shadow_dom.find_element_by_css_selector("#buttonRegister")
+        button_register.click()
+        time.sleep(3)
+
+    def should_be_message_about_wrong_sponsor_id(self, browser):
+        self.should_be_message_in_the_lower_right_corner(browser)
+        self.message_should_be_about_wrong_sponsor_id(browser)
+        # дописать проверки
+
--- test_login_page.py
+++ test_login_page.py
@@ -20,3 +20,14 @@ def test_guest_cant_sign_in_with_invalid_password(browser):
     password = "password_012345"
     page.sign_in_to_site(email, password, browser)
     page.should_be_message_about_wrong_password(browser)
+
+def test_guest_cant_register_with_invalid_sponsor_id(browser):
+    link = "https://dev-vkhvorostov.onlineoffice.pro/en-US"
+    page = LoginPage(browser, link)
+    page.open()
+    sponsor_id = "0900"
+    first_name = "Ivan"
+    last_name = "Ivanov"
+    email = "user10@example.com"
+    page.register_on_the_site(sponsor_id, first_name, last_name, email, browser)
+    page.should_be_message_about_wrong_sponsor_id(browser)
